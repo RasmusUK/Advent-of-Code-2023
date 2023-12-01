f389acc02fb7f4e4f2b6b255abf07acd2978ff14
RasmusUK <rasmus.kristensen@live.dk>
2023-12-01 16:25:31 +0100

    Day 1

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Reflection;
+
+Console.WriteLine("Welcome to code of advent");
+
+var day = 0;
+var parseSuccess = false;
+
+while (!parseSuccess)
+{
+    Console.WriteLine("Please enter day:");
+    parseSuccess = int.TryParse(Console.ReadLine(), out day);
+}
+
+var className = $"AdventOfCode.Days.Day{day}.Day{day}";
+var type = Type.GetType(className);
+
+if (type != null)
+{
+    var parameters = new object[]{ day };
+    var instance = Activator.CreateInstance(type, parameters);
+    var method = type.GetMethod("Run");
+
+    if (method != null)
+    {
+        method.Invoke(instance, null);
+    }
+    else
+    {
+        Console.WriteLine($"Method 'Run' not found in {className}");
+    }
+}
+else
+{
+    Console.WriteLine($"Class {className} not found");
+}
